bef0fb2d6858a620f5d4f8c3927613796f75ee0a
on4akh <on4akh@gmail.com>
2014-02-01 18:46:16 +0100

    added license info

--- smaster.c
+++ smaster.c
@@ -164,22 +164,7 @@ void *sMasterThread(){
 							if (buffer[90] !=0) {
 								memcpy(origC[slot],buffer+90,4);
 								syslog(LOG_NOTICE,"[sMaster]Replacing country code to %s",origC[slot]);
-						{description}
-    Copyright (C) {year} {fullname}
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.	} 
+							} 
 							else{ 
 								memcpy(origC[slot],buffer+64,4);
 							}
