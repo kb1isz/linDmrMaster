5ab2477b734a452df469fa78944450dcc258169e
on4akh <on4akh@gmail.com>
2014-02-08 14:37:58 +0100

    making it a deamon

--- main.c
+++ main.c
@@ -511,14 +511,45 @@ int loadTalkGroups(){
 
 int main(int argc, char**argv)
 {
+	
+	// Our process ID and Session ID
+	pid_t pid,sid;
+	
+	printf("Becoming a daemon...\n");
+	// Fork off the parent process
+	pid = fork();
+    if (pid < 0) {
+		exit(EXIT_FAILURE);
+    }
+	
+	// If we got a good PID, then we can exit the parent process. 
+    if (pid > 0) {
+		exit(EXIT_SUCCESS);
+    }
+	
+	// Change the file mode mask 
+    umask(0);
+
+	setlogmask (LOG_UPTO (LOG_NOTICE));
+	openlog("Master-server", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+	// Create a new SID for the child process 
+    sid = setsid();
+    if (sid < 0) {
+		// Log the failure 
+		exit(EXIT_FAILURE);
+    }
+	
+	// Close out the standard file descriptors 
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);	
+	
 	pthread_t thread;
 	int port;
 	int dbInit;
 	int i;
 	
-	//init syslog output
-	setlogmask (LOG_UPTO (LOG_NOTICE));
-	openlog("Master-server", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
 	db = openDatabase();
 	
