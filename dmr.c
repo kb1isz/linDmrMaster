e295e889fc78b12a8b1e259476b5862595d0c6b9
on4akh <on4akh@gmail.com>
2014-07-27 19:07:15 +0200

    further development dmr.c

--- dmr.c
+++ dmr.c
@@ -316,12 +316,12 @@ void *dmrListener(void *f){
 							break;
 						}
 						
-						if (slotType == 0x5555 && dmrState[slot] == DATA){ // 1/2 rate data continuation
+						if (slotType == 0x5555){ // 1/2 rate data continuation
 							dmrState[slot] = IDLE;
 							syslog(LOG_NOTICE,"[%i-%s]1/2 data continuation on slot %i src %i dst %i type %i",baseDmrPort + repPos,repeaterList[repPos].callsign,slot,srcId,dstId,callType);
 							break;
 						}
-						if (slotType == 0x6666 && dmrState[slot] == DATA){ // 3/4 rate data continuation
+						if (slotType == 0x6666){ // 3/4 rate data continuation
 							syslog(LOG_NOTICE,"[%i-%s]3/4 data continuation on slot %i src %i dst %i type %i",baseDmrPort + repPos,repeaterList[repPos].callsign,slot,srcId,dstId,callType);
 							dmrState[slot] = IDLE;
 							break;
